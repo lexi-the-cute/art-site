5095c6f9a37204bb15c09cc312ddcb44d440f66f
Alexis <120563837+alexisart@users.noreply.github.com>
2022-12-20 01:32:26 -0500

    Test Site

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+// Imports
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 
+
+// Constants
 const hostname = 'localhost';
 const port = 8080;
+
+
+// Variables
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 const server = http.createServer((req, res) => {
     console.log('Request for ' + req.url + ' by method ' + req.method);
@@ -11,12 +26,96 @@ const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	
-	res.write('Request for ' + req.url + ' by method ' + req.method);
-// 	fs.createReadStream(filePath).pipe(res);
+	var sql = "show databases;";
+	con.query(sql, function (err, result, fields) {
+		try {
+			if (err) throw err;
+			
+			console.log('Result: ' + JSON.stringify(result));
+			res.write('Result: ' + JSON.stringify(result));
+// 			fs.createReadStream(result).pipe(res);
+			
+			res.end();
+		} catch (err) {
+			console.error("Failed To Execute Query! " + err);
+// 			process.exit(1);
+		}
+	});
 	
-	res.end()
+// 	res.end();
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+// Functions
+function setupDatabase() {
+	// For setting up database from scratch if needed and possible
+// 	CREATE USER 'process.env.DB_USERNAME'@'process.env.DB_HOST' IDENTIFIED BY 'process.env.DB_PASSWORD';
+// 	GRANT INSERT, UPDATE, DELETE, SELECT ON process.env.DB_NAME.* to 'process.env.DB_USERNAME'@'process.env.DB_HOST';
+// 	CREATE DATABASE art;
+// 	USE art;
+// 	CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))
+}
+
+function startDatabase() {
+	// Try to connect to database, else fail out
+	
+	// Check If Missing Environment Variables For Connecting To MySql Database
+	if (process.env.DB_USERNAME === undefined || process.env.DB_PASSWORD === undefined || process.env.DB_HOST === undefined || process.env.DB_NAME === undefined) {
+		var missing_vars = "";
+		// TODO: Check if equivalent of Python list joining exists - See .join(",");
+		if (process.env.DB_USERNAME === undefined)
+			missing_vars += "DB_USERNAME, ";
+		if (process.env.DB_PASSWORD === undefined)
+			missing_vars += "DB_PASSWORD, ";
+		if (process.env.DB_HOST === undefined)
+			missing_vars += "DB_HOST, ";
+		if (process.env.DB_NAME === undefined)
+			missing_vars += "DB_NAME";
+		
+		console.error("Missing Environment Variable(s): " + missing_vars);
+		process.exit(1);
+	}
+	
+	con.connect(function(err) {
+		try {
+			if (err) throw err;
+				
+			console.log("Connected!");
+			setupDatabase();
+		} catch (err) {
+			console.error("Failed To Connect! " + err);
+			process.exit(1);
+		}
+	});
+}
+
+function startServer() {
+	// Start webserver
+	
+	server.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/`);
+		
+		// For Initial Connection - We May Lose Connection During Runtime
+		startDatabase();
+	});
+}
+
+function main() {
+	// We May Setup Configuration Here, We've Only Got The Server For Now Though
+	startServer();
+}
+
+
+// Loose Functions
+/* process.on('uncaughtException', function (err) {
+	console.error((new Date).toUTCString() + ' uncaughtException:', err.message);
+	console.error(err.stack);
+	process.exit(1);
+});*/
+
+
+// Initiator
+if (typeof require !== 'undefined' && require.main === module) {
+	// Call Main Function If Script Ran Directly
+	main();
+}
